0b0280ebf26fc76624cc010433c16410d252e33c
jpncaetano <caetanojpn@gmail.com>
2024-09-09 18:59:34 -0300

    Revisão 1.0

--- assets/js/telaAutenticacao.js
+++ assets/js/telaAutenticacao.js
@@ -5,6 +5,12 @@ const verificationOptions = document.getElementById('verification-options');
 const faceRecognitionOption = document.getElementById('face-recognition-option');
 const faceRecognitionSection = document.getElementById('face-recognition-section');
 
+// Função para abrir o modal
+function openModal() {
+    var modal = new bootstrap.Modal(document.getElementById('customModal'));
+    modal.show();
+}
+
 // Adiciona um evento de clique para simular o escaneamento do QR Code
 qrOption.addEventListener('click', function() {
     qrCodeSection.style.display = 'block';  // Mostra a seção do código QR
@@ -12,9 +18,8 @@ qrOption.addEventListener('click', function() {
 
     // Simula o processo de escaneamento do QR Code
     setTimeout(function() {
-        alert('Escaneamento concluído!');
-        window.location.href = '/assets/pages/visaoGeral.html';  // Redireciona para a página Visão Geral
-    }, 3000);  // Espera 3 segundos antes de redirecionar
+        openModal();  // Exibe o modal ao invés de alert()
+    }, 3000);  // Espera 3 segundos antes de exibir o modal
 });
 
 // Adiciona um evento de clique para simular o reconhecimento facial
@@ -24,7 +29,11 @@ faceRecognitionOption.addEventListener('click', function() {
 
     // Simula o processo de reconhecimento facial
     setTimeout(function() {
-        alert('Reconhecimento Facial bem-sucedido!');
-        window.location.href = '/assets/pages/visaoGeral.html';  // Redireciona para a página Visão Geral
-    }, 3000);  // Espera 3 segundos antes de redirecionar
+        openModal();  // Exibe o modal ao invés de alert()
+    }, 3000);  // Espera 3 segundos antes de exibir o modal
+});
+
+// Redirecionamento ao clicar no botão "Continuar"
+document.getElementById('continueButton').addEventListener('click', function() {
+    window.location.href = '/assets/pages/visaoGeral.html';  // Redireciona para a página Visão Geral
 });
